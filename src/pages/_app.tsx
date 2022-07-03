6733d9c7c81f4ba7ab3bdaec082a892e894651bd
Justin Sun <i@jsun969.cn>
2022-07-03 13:54:02 +0800

    :sparkles: init mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css';
+import createCache from '@emotion/cache';
+import type { EmotionCache } from '@emotion/react';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { pink, purple } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const theme = createTheme({
+  palette: {
+    primary: pink,
+    secondary: purple,
+  },
+});
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createCache({ key: 'css', prepend: true });
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+const MyApp = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
 
 export default MyApp;
