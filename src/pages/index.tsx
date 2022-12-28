2c82014efd8019041abb325809c507ef9691f52d
Justin Sun <i@jsun969.cn>
2022-12-28 18:57:58 +0800

    feat: 修改主页内容

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography, Link } from '@mui/material';
 import { type NextPage } from 'next';
 import Head from 'next/head';
 
@@ -9,7 +10,21 @@ const Home: NextPage = () => {
       <Head>
         <title>校园万能墙</title>
       </Head>
-      <h1 style={{ textAlign: 'center', marginTop: 300 }}>内测中</h1>
+      <Box
+        sx={{
+          textAlign: 'center',
+          marginTop: 25,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 4,
+        }}
+      >
+        <Typography variant="h4">内测中，欢迎站长注册</Typography>
+        <Typography>
+          QQ群：
+          <Link href="https://jq.qq.com/?_wv=1027&k=MSNyMu0O">153535519</Link>
+        </Typography>
+      </Box>
     </Layout>
   );
 };
