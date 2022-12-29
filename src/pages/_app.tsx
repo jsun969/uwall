bc04baf3431316f865088183160526d47fc5f9e0
Justin Sun <i@jsun969.cn>
2022-12-29 15:38:13 +0800

    feat: 添加路由切换加载条

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { EmotionCache } from '@emotion/react';
 import { CacheProvider } from '@emotion/react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, LinearProgress, ThemeProvider } from '@mui/material';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { useIsRouterChanging } from '../hooks/useIsRouterChanging';
 import createEmotionCache from '../lib/createEmotionCache';
 import theme from '../lib/theme';
 import { trpc } from '../lib/trpc';
@@ -23,15 +24,17 @@ const MyApp = ({
   pageProps: { session, ...pageProps },
   emotionCache = clientSideEmotionCache,
 }: MyAppProps) => {
+  const isRouterChanging = useIsRouterChanging();
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <SessionProvider session={session}>
+          {isRouterChanging && <LinearProgress />}
           <Component {...pageProps} />
         </SessionProvider>
       </ThemeProvider>
