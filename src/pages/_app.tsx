9d5066934a1f6572ab9f4b2f66f10974c86947cc
Justin Sun <i@jsun969.cn>
2023-04-30 15:09:09 +0800

    :lipstick: 添加 MUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,36 @@
-import { type AppType } from 'next/app';
-import '~/styles/globals.css';
+import type { EmotionCache } from '@emotion/react';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import createEmotionCache from '~/lib/createEmotionCache';
+import theme from '~/lib/theme';
 import { api } from '~/utils/api';
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = ({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
---
+++ src/pages/_document.tsx
@@ -0,0 +1,66 @@
+/**
+ * 对 MUI 进行服务端渲染
+ */
+import createEmotionServer from '@emotion/server/create-instance';
+import type { AppType } from 'next/app';
+import type { DocumentContext, DocumentProps } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import * as React from 'react';
+
+import type { MyAppProps } from './_app';
+import createEmotionCache from '~/lib/createEmotionCache';
+import theme from '~/lib/theme';
+
+interface MyDocumentProps extends DocumentProps {
+  emotionStyleTags: JSX.Element[];
+}
+
+export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
+  return (
+    <Html lang="zh-CN">
+      <Head>
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="emotion-insertion-point" content="" />
+        {emotionStyleTags}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (
+        App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>,
+      ) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
